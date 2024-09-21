e9ca775ba71227acc9bfe8e468357d91cf470422
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-09-21 11:26:14 +0300

    Pangram Problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.HashMap ;
+import java.util.Scanner ;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main (String[] args){
+        Scanner scan = new Scanner (System.in) ;
+        int n = scan.nextInt () ;
+        scan.nextLine() ;
+        String s = scan.nextLine() ;
 
+        HashMap <Character, Integer> map = new HashMap <> () ;
+        for (char c : s.toCharArray()) {
+            map.put (Character.toLowerCase(c), map.getOrDefault (Character.toLowerCase(c), 0) + 1) ;
+        }
+
+        int r = map.size() ;
+        if (r == 26) System.out.println ("YES") ;
+        else System.out.println ("NO") ;
     }
 }
