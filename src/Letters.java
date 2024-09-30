4b2b11e4f31179c517523dcbcd59093f79a62999
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-09-30 17:17:30 +0300

    first commit

---
+++ src/Letters.java
@@ -0,0 +1,17 @@
+import java.util.HashMap ;
+import java.util.Map ;
+import java.util.Scanner ;
+
+public class Letters{
+    public static void main (String[] args){
+        Scanner scan = new Scanner (System.in) ;
+        String a = scan.nextLine() ;
+        Map<Character, Integer> map = new HashMap <Character, Integer>() ;
+
+        for (char b : a.toCharArray ())
+            if (b >='a' && b <='z') map.put (b, map.getOrDefault (b, 0) + 1) ;
+
+        System.out.println (map.size ()) ;
+
+    }
+}
