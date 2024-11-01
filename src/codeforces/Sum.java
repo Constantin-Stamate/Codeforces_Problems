a20a4dda6f9ee288c9dc49b9b0a6d5a23548cbff
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-02 01:00:51 +0200

    first commit

---
+++ src/codeforces/Sum.java
@@ -0,0 +1,37 @@
+package codeforces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = scan.nextInt();
+            System.out.println(sumOfNumbers(a).size());
+            for (Integer num : sumOfNumbers(a)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static List<Integer> sumOfNumbers(int n) {
+        int a = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (n != 0) {
+            if (n % 10 != 0) list.add(n % 10 * a);
+            a *= 10;
+            n /= 10;
+        }
+
+        List<Integer> sum = list.stream()
+                .toList()
+                .reversed();
+
+        return sum;
+    }
+}
