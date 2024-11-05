32c6179fefd6d3fdd587bb1fff7fc898bfac63e5
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-05 14:22:07 +0200

    first commit

---
+++ src/codeforces/Cube.java
@@ -0,0 +1,23 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class Cube {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(heightOfThePyramid(n));
+    }
+
+    public static int heightOfThePyramid(int n) {
+        int rez = 0, s = 1, p = 2;
+
+        while (s <= n) {
+            rez++;
+            n -= s;
+            s += p;
+            p++;
+        }
+        return rez;
+    }
+}
