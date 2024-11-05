3effbe8771a2ba24ac5c8657d0d3e313c5896b52
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-05 10:26:33 +0200

    first commit

---
+++ src/codeforces/Unique.java
@@ -0,0 +1,23 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class Unique {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            System.out.println(uniqueNumber(a, b, c));
+        }
+    }
+
+    public static int uniqueNumber(int a, int b, int c) {
+        if (a == b) return c;
+        else
+            if (a == c) return b;
+        else return a;
+    }
+}
