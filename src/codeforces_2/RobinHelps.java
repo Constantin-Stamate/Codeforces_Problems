8b00769a238ee165bc08376ef12fdffb27b3f195
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-08 19:19:24 +0200

    first commit

---
+++ src/codeforces_2/RobinHelps.java
@@ -0,0 +1,39 @@
+package codeforces_2;
+
+import java.util.Scanner;
+
+public class RobinHelps {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scan.nextInt();
+            int k = scan.nextInt();
+            int[] arr = createArray(n);
+
+            System.out.println(numberOfPeople(n, arr, k));
+        }
+    }
+
+    public static int[] createArray(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        return arr;
+    }
+
+    public static int numberOfPeople(int n, int[] arr, int k) {
+        int count = 0;
+        int rez = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= k) count += arr[i];
+            else if (arr[i] == 0 && count >= 1){
+                count--;
+                rez++;
+            }
+        }
+        return rez;
+    }
+}
