d4735deac1db8fbe0e90c67d98166a5d486d3c0d
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-09 23:32:56 +0200

    first commit

---
+++ src/codeforces_2/Letter.java
@@ -0,0 +1,28 @@
+package codeforces_2;
+
+import java.util.Scanner;
+
+public class Letter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 0; i < t; i++) {
+            String s = scan.next();
+            verificationString(s);
+        }
+    }
+
+    public static void verificationString(String s) {
+        if (s.equalsIgnoreCase("YES")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+
+
+    }
+}
