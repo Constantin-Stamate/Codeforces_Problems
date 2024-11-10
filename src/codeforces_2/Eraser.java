ff6d9efcbbadff7610f902a0df864773d41a40eb
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-10 23:45:39 +0200

    first commit

---
+++ src/codeforces_2/Eraser.java
@@ -0,0 +1,33 @@
+package codeforces_2;
+
+import java.util.Scanner;
+
+public class Eraser {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scan.nextInt();
+            int k = scan.nextInt();
+            scan.nextLine();
+            String s = scan.next();
+            System.out.println(numberOfOperations(n, k, s));
+        }
+    }
+
+    public static int numberOfOperations(int n, int k, String s) {
+        int count = 0;
+        StringBuilder rez = new StringBuilder(s);
+        for (int i = 0; i < n; i++) {
+            if (rez.charAt(i) == 'B') {
+                for (int j = 0; j < k; j++) {
+                    if (i + j < n) rez.setCharAt(i + j, 'W');
+                    else break;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
