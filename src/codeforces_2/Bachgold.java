11ee3c70fd2e43fa86eff448c2e5e6f2e0c5332a
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-11 13:25:11 +0200

    first commit

---
+++ src/codeforces_2/Bachgold.java
@@ -0,0 +1,26 @@
+package codeforces_2;
+
+import java.util.Scanner;
+
+public class Bachgold {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        primeFactors(n);
+    }
+
+    public static void primeFactors(int n) {
+        if (n % 2 == 0) {
+            System.out.println(n / 2);
+            for (int i = 0; i < n / 2; i++) {
+                System.out.print(2 + " ");
+            }
+        } else {
+            System.out.println(n / 2);
+            for (int i = 0; i < (n - 3) / 2; i++) {
+                System.out.print(2 + " ");
+            }
+            System.out.print(3 + " ");
+        }
+    }
+}
