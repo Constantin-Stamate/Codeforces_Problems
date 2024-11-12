76d00c104d1c6d3cdf65431982939cc2c673ad94
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-12 11:32:42 +0200

    first commit

---
+++ src/codeforces_2/Maximum.java
@@ -0,0 +1,33 @@
+package codeforces_2;
+
+import java.util.Scanner;
+
+public class Maximum {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scan.nextInt();
+            int[] arr = createArray(n);
+            System.out.println(findMinimum(arr, n));
+        }
+    }
+
+    public static int[] createArray(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        return arr;
+    }
+
+    public static int findMinimum(int[] arr, int n) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 1; i++) {
+            int maxPair = Math.max(arr[i], arr[i + 1]);
+            min = Math.min(min, maxPair);
+        }
+        return min - 1;
+    }
+
+}
