4fd08f153fd5588593f95e7754dc239a14fe5b90
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-12 10:40:48 +0200

    first commit

---
+++ src/codeforces_2/Frequency.java
@@ -0,0 +1,33 @@
+package codeforces_2;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Frequency {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            String s = scan.next();
+            System.out.println(frequencyOfCharacters(s));
+        }
+    }
+
+    public static Character frequencyOfCharacters(String s) {
+        int frequency = 0;
+        char w = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (Character key : map.keySet()) {
+            if (map.get(key) > frequency) {
+                frequency = map.get(key);
+                w = key;
+            }
+        }
+
+        return w;
+    }
+}
