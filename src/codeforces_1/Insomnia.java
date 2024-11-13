36df8194d7633aaedf0d257d8aa04b735243ad6e
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-13 23:32:56 +0200

    find the number of integers divisible by given values

--- src/codeforces_1/Insomnia.java
+++ src/codeforces_1/Insomnia.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class Insomnia {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int k = scan.nextInt();
         int l = scan.nextInt();
         int m = scan.nextInt();
         int n = scan.nextInt();
         int d = scan.nextInt();
 
-        int cnt = 0;
-
-        for (int i = 1; i <= d; i++)
-            if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) cnt++;
-
+        int cnt = countSleeplessDays(k, l, m, n, d);
         System.out.println(cnt);
     }
+
+    public static int countSleeplessDays(int k, int l, int m, int n, int d) {
+        int cnt = 0;
+        for (int i = 1; i <= d; i++) {
+            if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
+
