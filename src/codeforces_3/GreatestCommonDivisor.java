6bd28f95b5867e5cdb765be857dbeefb5f448892
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-08-05 23:07:24 +0300

    compute factorial of smaller number between two inputs

--- src/codeforces_3/GreatestCommonDivisor.java
+++ src/codeforces_3/GreatestCommonDivisor.java
@@ -1,4 +1,25 @@
 package codeforces_3;
 
+import java.util.Scanner;
+
 public class GreatestCommonDivisor {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] main) {
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(gcdOfFactorials(a, b));
+    }
+
+    public static int gcdOfFactorials(int a, int b) {
+        int minValue = Math.min(a, b);
+        int fact = 1;
+
+        for (int i = 2; i <= minValue; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
 }
