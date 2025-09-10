e8555ce7db40a34b5867892cccd77eaf3d96b90f
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-09-10 22:03:53 +0300

    add solution for watermelon problem

--- src/codeforces_3/Watermelon.java
+++ src/codeforces_3/Watermelon.java
@@ -1,4 +1,29 @@
 package codeforces_3;
 
+import java.util.Scanner;
+
 public class Watermelon {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int w = readInput();
+        String result = solve(w);
+        printResult(result);
+    }
+
+    private static int readInput() {
+        return scanner.nextInt();
+    }
+
+    private static String solve(int w) {
+        if (w > 2 && w % 2 == 0) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    private static void printResult(String result) {
+        System.out.println(result);
+    }
 }
