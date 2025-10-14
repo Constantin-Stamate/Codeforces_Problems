19686a53eeb8308ca88c0b7a85553b56d3305dcc
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-10-14 11:30:24 +0300

    add solution with iteration count calculation and array handling

---
+++ src/codeforces_5/Lever.java
@@ -0,0 +1,41 @@
+package codeforces_5;
+
+import java.util.Scanner;
+
+public class Lever {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = readArraySize();
+            int[] a = readArray(n);
+            int[] b = readArray(n);
+            System.out.println(computeIterations(a, b, n));
+        }
+    }
+
+    public static int readArraySize() {
+        return scan.nextInt();
+    }
+
+    public static int[] readArray(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        return arr;
+    }
+
+    public static int computeIterations(int[] firstArray, int[] secondArray, int n) {
+        int over = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (firstArray[i] > secondArray[i]) {
+                over += firstArray[i] - secondArray[i];
+            }
+        }
+
+        return over + 1;
+    }
+}
